5b4e72b8e85081f1974f591fbfccbd46f222cdaf
dan.bourque <dan.bourque@2a4c9ac2-3454-11de-a81c-fde7d73ceb89>
2009-04-29 15:03:54 +0000

    Added a reference to the new EditingViewController instance that we'll invoke when the user wishes to edit an entry.

--- GoogleCalendar/Classes/RootViewController.h
+++ GoogleCalendar/Classes/RootViewController.h
@@ -13,15 +13,20 @@
 #define KEY_EVENTS @"events"
 #define KEY_TICKET @"ticket"
 
+// Forward declaration of the editing view controller's class for the compiler.
+@class EditingViewController;
+
 @interface RootViewController : UITableViewController{
   UINavigationBar* navigationBar;
   NSMutableArray *data;
   GDataServiceGoogleCalendar *googleCalendarService;
+  EditingViewController *editingViewController;
 }
 
 - (void)fetchCalendars;
 
 @property (nonatomic, retain) UINavigationBar* navigationBar;
 @property (nonatomic, retain) NSMutableArray *data;
+@property (nonatomic, retain) EditingViewController *editingViewController;
 
 @end
