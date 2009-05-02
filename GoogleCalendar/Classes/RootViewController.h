e3a746bd45d8b2ab070a359a1bf9146ff50f4813
dan.bourque <dan.bourque@2a4c9ac2-3454-11de-a81c-fde7d73ceb89>
2009-05-02 18:07:36 +0000

    I determine whether the user has the ability to add/remove events by the existence of its ACL link.  For each calendar, I add a KEY_EDITABLE key in its dictionary if it has one.  This allows me to quickly test if it's editable elsewhere in my code.

--- GoogleCalendar/Classes/RootViewController.h
+++ GoogleCalendar/Classes/RootViewController.h
@@ -9,9 +9,15 @@
 #import <UIKit/UIKit.h>
 #import "GDataCalendar.h"
 
+// These keys are used to lookup elements in our dictionaries.
 #define KEY_CALENDAR @"calendar"
 #define KEY_EVENTS @"events"
 #define KEY_TICKET @"ticket"
+#define KEY_EDITABLE @"editable"
+// Currently, the app just shows calendar entries from 15 days ago to 31 days from now.
+// Ideally, we would instead use similar controls found in Google Calendar web interface, or even iCal's UI.
+#define PERIOD_15_DAYS 60*60*24*15
+#define PERIOD_31_DAYS 60*60*24*31
 
 // Forward declaration of the editing view controller's class for the compiler.
 @class EditingViewController;
