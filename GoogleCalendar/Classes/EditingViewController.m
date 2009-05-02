f4cf2b80502bcd3cc586057ab4615258b5e80c81
dan.bourque <dan.bourque@2a4c9ac2-3454-11de-a81c-fde7d73ceb89>
2009-05-02 04:12:50 +0000

    Instead of extending the UITableViewController, I now extend the UIViewController and use the nib's key-value bindings instead.

--- GoogleCalendar/Classes/EditingViewController.m
+++ GoogleCalendar/Classes/EditingViewController.m
@@ -6,36 +6,28 @@
 //  Copyright Dan Bourque 2009. All rights reserved.
 //
 #import "EditingViewController.h"
-#import "EditableCell.h"
 
 @implementation EditingViewController
 
-@synthesize editingItem, editingItemCopy, editingContent, sectionName, headerView;
+@synthesize editingItem, editingItemCopy, editingContent, calendarName, headerView;
 
 - (void)viewWillAppear:(BOOL)animated{
-  self.title = [NSString stringWithFormat:@"Editing %@", sectionName];
+  self.title = calendarName;
   // If the editing item is nil, that indicates a new item should be created
   if( !editingItem ){
-    self.editingItem = [NSMutableDictionary dictionaryWithObjectsAndKeys:@"", @"Name", nil];
+    self.editingItem = [NSMutableDictionary dictionaryWithObjectsAndKeys:@"when?", KEY_WHEN, @"what?", KEY_WHAT, @"where?", KEY_WHERE, nil];
     // Rather than immediately add the new item to the content array, set a flag.
     // When the user saves, add the item then; if the user cancels, no action is needed.
     newItem = YES;
   }
-  [self.tableView reloadData];
-  if( !nameCell )
-    nameCell = [[EditableCell alloc] initWithFrame:CGRectZero reuseIdentifier:@"NameCell"];
-  nameCell.textField.placeholder = sectionName;
-  nameCell.textField.text = [editingItem valueForKey:@"Name"];
-  // Starts editing in the name field and shows the keyboard
-  [nameCell.textField becomeFirstResponder];
-}
-
-- (void)viewWillDisappear:(BOOL)animated{
-  // hides the keyboard
-  [nameCell.textField resignFirstResponder];
+  
+  what.text = [editingItem valueForKey:KEY_WHAT];
+  where.text = [editingItem valueForKey:KEY_WHERE];
+  when.date = [editingItem valueForKey:KEY_WHEN];
 }
 
 - (void)viewDidLoad{
+ 	self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
   // use an empty view to position the cells in the vertical center of
   // the portion of the view not covered by the keyboard
   self.headerView = [[[UIView alloc] initWithFrame:CGRectMake( 0, 0, 5, 100 )] autorelease];
@@ -46,6 +38,16 @@
   self.navigationItem.rightBarButtonItem = [[UIBarButtonItem  alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSave
                                                                                           target:self
                                                                                           action:@selector( save )];
+  
+  what.clearButtonMode = UITextFieldViewModeWhileEditing;
+	what.delegate = self;
+  where.clearButtonMode = UITextFieldViewModeWhileEditing;
+	where.delegate = self;  
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+	[textField resignFirstResponder];
+	return NO;
 }
 
 // When we set the editing item, we also make a copy in case edits
@@ -65,7 +67,10 @@
 
 - (IBAction)save{
   // save edits to the editing item and add new item to the content.
-  [editingItem setValue:nameCell.textField.text forKey:@"Name"];
+  [editingItem setValue:what.text forKey:KEY_WHAT];
+  [editingItem setValue:where.text forKey:KEY_WHERE];
+  [editingItem setValue:when.date forKey:KEY_WHEN];
+
   if( newItem ){
     [editingContent addObject:editingItem];
     newItem = NO;
@@ -77,40 +82,9 @@
   [editingItem release];
   [editingItemCopy release];
   [editingContent release];
-  [sectionName release];
+  [calendarName release];
   [headerView release];
   [super dealloc];
-}
-
-- (void)tableView:(UITableView *)aTableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-  [aTableView deselectRowAtIndexPath:indexPath animated:YES];
-}
-
-// Have an accessory view for the second section only
-- (UITableViewCellAccessoryType)tableView:(UITableView *)aTableView accessoryTypeForRowWithIndexPath:(NSIndexPath *)indexPath{
-  return indexPath.section==0?UITableViewCellAccessoryNone:UITableViewCellAccessoryDisclosureIndicator;
-}
-
-// Make the header height in the first section 45 pixels
-- (CGFloat)tableView:(UITableView *)aTableView heightForHeaderInSection:(NSInteger)section{
-  return section==0?45.0:10.0;
-}
-
-// Show a header for only the first section
-- (UIView *)tableView:(UITableView *)aTableView viewForHeaderInSection:(NSInteger)section{
-  return section==0?headerView:nil;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)aTableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-  return indexPath.section==0?nameCell:nil;
-}
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)aTableView{
-  return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)aTableView numberOfRowsInSection:(NSInteger)section{
-  return 1;
 }
 
 @end
