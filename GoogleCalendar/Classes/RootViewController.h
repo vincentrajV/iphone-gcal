1be52235a63b2097b8d857bd05328110f58a9099
dan.bourque <dan.bourque@2a4c9ac2-3454-11de-a81c-fde7d73ceb89>
2009-05-04 06:17:12 +0000

    Instead of setting the GData Calendar service to automatically follow next links, I do it myself after each batch of returned entries.  This gives the app a chance to process user input, and incrementally show the tables as they grow.

--- GoogleCalendar/Classes/RootViewController.h
+++ GoogleCalendar/Classes/RootViewController.h
@@ -14,10 +14,6 @@
 #define KEY_EVENTS @"events"
 #define KEY_TICKET @"ticket"
 #define KEY_EDITABLE @"editable"
-// Currently, the app just shows calendar entries from 15 days ago to 31 days from now.
-// Ideally, we would instead use similar controls found in Google Calendar web interface, or even iCal's UI.
-#define PERIOD_15_DAYS 60*60*24*15
-#define PERIOD_31_DAYS 60*60*24*31
 
 // Forward declaration of the editing view controller's class for the compiler.
 @class EditingViewController;
@@ -27,14 +23,13 @@
   NSMutableArray *data;
   GDataServiceGoogleCalendar *googleCalendarService;
   EditingViewController *editingViewController;
-  NSString *statusMessage;
 }
 
-- (void)refresh;
+// We declare accessor for the googleCalendarService because we need to access it from the EditingViewController class.
+@property (nonatomic, retain) GDataServiceGoogleCalendar *googleCalendarService;
 
-@property (nonatomic, retain) UINavigationBar* navigationBar;
-@property (nonatomic, retain) NSMutableArray *data;
-@property (nonatomic, retain) EditingViewController *editingViewController;
-@property (nonatomic, retain) NSString *statusMessage;
+- (void)refresh;
+- (void)insertCalendarEvent:(GDataEntryCalendarEvent *)newEvent toCalendar:(GDataEntryCalendar *)calendar;
+- (void)updateCalendarEvent:(GDataEntryCalendarEvent *)newEvent toCalendar:(GDataEntryCalendar *)calendar;
 
 @end
